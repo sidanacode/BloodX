9df8583c191cee16e8c8601810e030e4c43bd289
sidanacode <sidanacode@gmail.com>
2025-05-27 17:19:36 +0530

    working on method for donor

--- db/methods.py
+++ db/methods.py
@@ -9,7 +9,7 @@ Session = sessionmaker(bind=engine,autoflush=True,autocommit=False)
 def add_donor(db,usr_name,usr_dob,usr_gender,usr_bloodgroup,usr_mobnum,usr_pincode,usr_donated,usr_last_donation_date):
     if isinstance(usr_dob,str):
         usr_dob = datetime.strptime(usr_dob,"%d-%m-%Y").date()
-    if isinstance(last_donation_date,str):
-        last_donation_date = datetime.strptime(last_donation_date,"%d-%m-%Y").date()
+    if isinstance(usr_last_donation_datelast_donation_date,str):
+        usr_last_donation_datelast_donation_date = datetime.strptime(usr_last_donation_date,"%d-%m-%Y").date()
     
-    new_donor = Donor(name=usr_name,dob=usr_dob,gender=usr_gender,blood_group=usr_bloodgroup,mobile_no=usr_mobnum,pin_code=usr_pincode,)
+    new_donor = Donor(name=usr_name,dob=usr_dob,gender=usr_gender,blood_group=usr_bloodgroup,mobile_no=usr_mobnum,pin_code=usr_pincode,last_donation_date=usr_last_donation_date,donated=usr_donated)
